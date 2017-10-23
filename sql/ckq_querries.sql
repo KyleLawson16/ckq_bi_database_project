132756100200c810ca7c1baa6ca4eddd4dd79032
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2017-10-22 20:58:23 -0400

    Added aggregate querry

--- sql/ckq_querries.sql
+++ sql/ckq_querries.sql
@@ -1,3 +1,7 @@
+/* Team Name: CKQ Dealership Members: Chris Post, Kyle Lawson, Quang Tran */
+
+use CKQDealership
+
 /* Queries that list out all table data for all four tables */
 
 select *
@@ -12,6 +16,13 @@ from Employees
 select *
 from Vehicles
 
+/** Aggregate function: Find the Total Number of Sales and the Total Sale for each car brand in the year 2016 **/
+
+SELECT V.Make AS 'Car Brand', COUNT(S.VehicleID) AS 'Number of Sales', SUM(S.SaleTotal) AS 'Total Sale'
+FROM Vehicles AS V 
+INNER JOIN SALES AS S ON V.VehicleID = S.VehicleID
+WHERE YEAR(S.SaleDate) = 2016
+GROUP BY V.Make;
 
 
 /* Show each sale made by each employee */
@@ -22,8 +33,7 @@ select E.FirstName,
 	S.SaleTotal
 from Employees as E
 inner join Sales as S
-	on S.EmployeeID = E.EmployeeID
-
+	on S.EmployeeID = E.EmployeeID;
 
 
 /* Show each vehicle that was part of a sale totalling over $20,000 */
@@ -33,4 +43,4 @@ select V.Make,
 	S.SaleTotal
 from Sales as S
 left outer join Vehicles as V
-	on V.VehicleID = S.VehicleID
+	on V.VehicleID = S.VehicleID;
